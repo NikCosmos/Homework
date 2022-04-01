d4b48aff00bc992a31cfc747cc8904f723f2eed3
Nik Gor <nikcosmos@gmail.com>
2022-04-01 21:07:04 +0300

    homework#3

---
+++ script3.js
@@ -0,0 +1,63 @@
+const operator = getOperator();
+
+const a = getOperand('Первый операнд');
+
+const b = getOperand('Второй операнд');
+
+let result = calculate(a, b, operator);
+
+showResult();
+
+function getOperator() {
+   let val;
+   do {
+      val = prompt('Выбери оператор');
+   } while (checkOperator(val));
+   return val;
+}
+
+function checkOperator(operator) {
+   if (
+      operator == '+' ||
+      operator == '-' ||
+      operator == '*' ||
+      operator == '/'
+   ) {
+      return false;
+   } else {
+      return true;
+   }
+}
+
+function getOperand(messange) {
+   let val;
+   do {
+      val = prompt(messange);
+   } while (checkOperand(val));
+   return +val;
+}
+
+function checkOperand(operand) {
+   if (!isNaN(operand)) {
+      return false;
+   } else {
+      return true;
+   }
+}
+
+function calculate(x, y, action) {
+   switch (action) {
+      case '+':
+         return x + y;
+      case '-':
+         return x - y;
+      case '*':
+         return x * y;
+      case '/':
+         return x / y;
+   }
+}
+
+function showResult() {
+   alert(`${a} ${operator} ${b} = ${result}`);
+}
