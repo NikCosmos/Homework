306b2a16f3a4eeb3a04b6c7eedba676708167162
Nik Gor <nikcosmos@gmail.com>
2022-04-04 21:28:09 +0300

    homework#4

---
+++ script4.js
@@ -0,0 +1,63 @@
+const operator = getOperator();
+
+const operand = getOperand();
+
+const result = calculate(operator, operand);
+
+showResult(operator, operand);
+
+function getOperator() {
+   let op;
+
+   do {
+      op = prompt('Укажите оператор');
+   } while (isValueOperator(op));
+
+   return op;
+}
+
+function isValueOperator(val) {
+   return val !== '+' && val !== '-' && val !== '/' && val !== '*';
+}
+
+function getOperand() {
+   let operand;
+   do {
+      operand = prompt('Числа');
+   } while (operand === null || operand === '');
+   return splitOperand(operand);
+}
+
+function splitOperand(operand) {
+   let arr = operand.split(',');
+   return arr;
+}
+
+function calculate(x, y) {
+   let sum = +y[0];
+   for (i = 1; i < y.length; i++) {
+      switch (x) {
+         case '+':
+            sum += +y[i];
+            break;
+         case '-':
+            sum -= +y[i];
+            break;
+         case '*':
+            sum *= +y[i];
+            break;
+         case '/':
+            sum /= +y[i];
+            break;
+      }
+   }
+   return sum;
+}
+
+function showResult(x, y) {
+   let res = +y[0];
+   for (i = 1; i < y.length; i++) {
+      res = `${res} ${x} ${+y[i]}`;
+   }
+   return alert(`${res} = ${result}`);
+}
